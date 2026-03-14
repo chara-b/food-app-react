38606fd77878dc013ad994d10bd90cc4a6e16848
chara <chara.blgr@gmail.com>
2026-03-14 14:54:45 +0200

    fix: scrollbar fixed at bin page to fit the screen

--- src/pages/Bin.jsx
+++ src/pages/Bin.jsx
@@ -10,6 +10,11 @@ function Bin() {
   const disabledProductsLoaded = useLoaderData();
   console.log("binData", disabledProductsLoaded);
 
+  // const navigation = useNavigation();
+  // const isLoading = navigation.state === "loading";
+
+  // const location = useLocation();
+
   const actionBtns = useMemo(
     () => [
       {
@@ -40,14 +45,9 @@ function Bin() {
   );
 
   return (
-    <div className="flex flex-col gap-4 w-full h-screen">
-      {/* {!isLoading && location.pathname.includes("bin") && ( */}
-      <ProductListBin className="w-full" actionBtns={actionBtns} colsCount="3">
-        <Paginator count={disabledProductsLoaded.length} perPage={5} />
-      </ProductListBin>
-      {/*  )} */}
-      {/* {isLoading && <Spinner />} */}
-    </div>
+    <ProductListBin className="w-full" actionBtns={actionBtns} colsCount="3">
+      <Paginator count={disabledProductsLoaded.length} perPage={5} />
+    </ProductListBin>
   );
 }
 
